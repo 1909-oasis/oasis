53de6ceb218a1529bc91d89ce99a8585670d61a1
Samantha Lastig <slastig13@gmail.com>
2019-11-22 17:01:26 -0500

    moving from TS to JS

---
+++ src/pages/Details.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import {
+  IonBackButton,
+  IonButtons,
+  IonHeader,
+  IonPage,
+  IonToolbar,
+  IonTitle,
+  IonContent
+} from '@ionic/react'
+
+class Details extends React.Component {
+  render() {
+    return (
+      <IonPage>
+        <IonHeader>
+          <IonToolbar>
+            <IonButtons slot="start">
+              <IonBackButton defaultHref="/tab2" />
+            </IonButtons>
+            <IonTitle>Detail</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent>
+          <p>Details</p>
+        </IonContent>
+      </IonPage>
+    )
+  }
+}
+
+export default Details
---
+++ src/pages/Tab3.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import {
+  IonHeader,
+  IonToolbar,
+  IonPage,
+  IonTitle,
+  IonContent
+} from '@ionic/react'
+
+class Tab3Page extends React.Component {
+  render() {
+    return (
+      <IonPage>
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle>Tab Three</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent />
+      </IonPage>
+    )
+  }
+}
+
+export default Tab3Page
